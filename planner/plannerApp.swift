91e20e2a007d1be58c295bc2d11fc1d19543dba2
jaehyeons <marindec16@gmail.com>
2024-04-15 22:33:07 +0900

    Initial Commit

---
+++ planner/plannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  plannerApp.swift
+//  planner
+//
+//  Created by JaehyeonS on 4/15/24.
+//
+
+import SwiftUI
+
+@main
+struct plannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
